0eca8de872eb50aa727d8aae4ab35df5afc6f4a5
greenrace <73060786+greenrace666@users.noreply.github.com>
2026-02-17 17:31:00 +0530

    newcv

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FileDown, Award, Book } from 'lucide-react';
-import pdf from "/src/cvpdf.pdf"
+import pdf from "/src/cvnew.pdf"
 const Resume = () => {
   const certifications = [
     {
